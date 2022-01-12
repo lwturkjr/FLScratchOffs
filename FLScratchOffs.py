14a4bf1b910d850ec69fa5b64491c9ba2fcc2eaf
Lloyd W Turk Jr <xerodill@gmail.com>
2022-01-12 16:01:26 -0500

    Push

--- FLScratchOffs.py
+++ FLScratchOffs.py
@@ -4,7 +4,13 @@ from bs4 import BeautifulSoup as bs
 
 URL = "https://www.flalottery.com/"
 rem_prizes = "remainingPrizes"
+ticket_id = "scratch-offsGameDetails?gameNumber="
 
 page = re.get(URL+rem_prizes)
 
 soup = bs(page.content, "html.parser")
+
+game_num = soup.find_all("td", class_="column1")
+
+for game_num in game_num:
+    print(game_num, end="\n")
