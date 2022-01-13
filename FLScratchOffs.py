45bc3e31babad59a85a925c7c9c75951fe9471f1
lwturkjr <lwturkjr@SgtSegFault.home>
2022-01-12 19:22:26 -0500

    Push

--- FLScratchOffs.py
+++ FLScratchOffs.py
@@ -4,13 +4,17 @@ from bs4 import BeautifulSoup as bs
 
 URL = "https://www.flalottery.com/"
 rem_prizes = "remainingPrizes"
-ticket_id = "scratch-offsGameDetails?gameNumber="
+ticket_url = "scratch-offsGameDetails?gameNumber="
 
 page = re.get(URL+rem_prizes)
 
 soup = bs(page.content, "html.parser")
 
-game_num = soup.find_all("td", class_="column1")
 
-for game_num in game_num:
-    print(game_num, end="\n")
+table = soup.find("table")
+
+headers = [header.text for header in table.find_all('th')]
+results = [{headers[i]: cell.text.strip() for i, cell in enumerate(row.find_all('td'))}for row in table.find_all('tr')]
+
+for x in results:
+    print(x, end="\n")
